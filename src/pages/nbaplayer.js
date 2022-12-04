e888ff9d8e977bdafa11c7c54ed295f9d034a2e0
Tejas Singh <55640297+singhtejas@users.noreply.github.com>
2022-12-04 03:49:41 -0700

    Update nbaplayer.js

--- src/pages/nbaplayer.js
+++ src/pages/nbaplayer.js
@@ -1,8 +1,14 @@
 import React, {useState, useEffect} from 'react';
 const Nbaplayer = () => {
     const [players, setPlayers] = useState([]);
+    const [teams,setTeams] = useState("All");
+    const [team,setTeam] = useState([]);
+    const [positions, setPositions] = useState("All");
+    const [position, setPosition] = useState([]);
 
-    async function onEnter(){
+    // cost [,] = useState([]);
+
+    async function showAllPlayers(){
         try{
             const response = await fetch('http://localhost:5000/total_players')
             const parseResponse = await response.json();
@@ -12,12 +18,48 @@ const Nbaplayer = () => {
             console.error(error.message)
         }
     }
+
+    async function getTeamNames(){
+        try {
+            const response = await fetch('http://localhost:5000/teams')
+            const parseResponse = await response.json();
+            setTeams(parseResponse);
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+    async function getPositions(){
+        try {
+            const response = await fetch('http://localhost:5000/positions')
+            const parseResponse = await response.json();
+            setPositions(parseResponse);
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
     useEffect(() => {
-        onEnter();
+        showAllPlayers();
       }, []);
     
+      useEffect(() => {
+        getTeamNames();
+      }, []);
+
+      useEffect(() => {
+        getPositons();
+      }, []);
+
     return (
     <div className='Players'>
+        <label>Team Name:</label>
+        <select name = 'teamValue' size={teams.size + 1} onChange={e => setTeam(e.target.value)}>
+            <option value="All">All</option>
+            {teams.map(team => (
+                <option value={team.team_name}>{team.team_name}</option>
+            ))}
+        </select>
        <table className='tablePlayer'>
             <thead>
                 <th>Name</th>
