db67b790ff1dee190ab943289f746e8bd033e40c
Tejas Singh <55640297+singhtejas@users.noreply.github.com>
2022-12-04 00:43:44 -0700

    fixed formatting of return

--- src/pages/nbaplayer.js
+++ src/pages/nbaplayer.js
@@ -1,27 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 
 const Nbaplayer = () => {
-<<<<<<< Updated upstream
-    return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-
-            }}
-        >
-            <h1>This is an NBA database</h1>
-=======
     const [players, setPlayers] = useState([]);
 
     async function onEnter(e){
         try{
             e.preventDefault();
             const response = await fetch('http://localhost:5000/total_players')
-            const parseResponse = await reponse.json();
+            const parseResponse = await response.json();
 
             setPlayers(parseResponse);
         }
@@ -29,25 +16,22 @@ const Nbaplayer = () => {
             console.error(error.message)
         }
     }
-
-
-        return (
-        <div className='Players'>
-            <table className="table my-5">
-                <tbody>
-                    {players.map(player => (
-                        <tr key={player.player_name}>
-                            <td>{player.player_name}</td>
-                            <td>{player.team_name}</td>
-                            <td>{player.player_position}</td>
-                            <td>{player.player_age}</td>
-                        </tr>
-                    ))
-                    }
-                </tbody>
-            </table>    
->>>>>>> Stashed changes
-        </div>
+    return (
+    <div className='Players'>
+        <table className="table my-5">
+            <tbody>
+                {players.map(player => (
+                    <tr key={player.player_name}>
+                        <td>{player.player_name}</td>
+                        <td>{player.team_name}</td>
+                        <td>{player.player_position}</td>
+                        <td>{player.player_age}</td>
+                    </tr>
+                ))
+                }
+            </tbody>
+        </table>    
+    </div>
     );
 };
 
