8469daedd4a00f25353db320a1050ff834d5b793
Tejas Singh <55640297+singhtejas@users.noreply.github.com>
2022-12-04 00:38:36 -0700

    adding player page data fetch

--- src/pages/nbaplayer.js
+++ src/pages/nbaplayer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 
 const Nbaplayer = () => {
+<<<<<<< Updated upstream
     return (
         <div
             style={{
@@ -13,6 +14,39 @@ const Nbaplayer = () => {
             }}
         >
             <h1>This is an NBA database</h1>
+=======
+    const [players, setPlayers] = useState([]);
+
+    async function onEnter(e){
+        try{
+            e.preventDefault();
+            const response = await fetch('http://localhost:5000/total_players')
+            const parseResponse = await reponse.json();
+
+            setPlayers(parseResponse);
+        }
+        catch (error) {
+            console.error(error.message)
+        }
+    }
+
+
+        return (
+        <div className='Players'>
+            <table className="table my-5">
+                <tbody>
+                    {players.map(player => (
+                        <tr key={player.player_name}>
+                            <td>{player.player_name}</td>
+                            <td>{player.team_name}</td>
+                            <td>{player.player_position}</td>
+                            <td>{player.player_age}</td>
+                        </tr>
+                    ))
+                    }
+                </tbody>
+            </table>    
+>>>>>>> Stashed changes
         </div>
     );
 };
